85da9c7a33a1e36072c827a2f3f34b9cbda9295a
Claudio Fahey <claudio.fahey@dell.com>
2021-04-21 05:27:45 +0000

    Add pravegatc3.sh
    Signed-off-by: Claudio Fahey <claudio.fahey@dell.com>

---
+++ scripts/pravegatc3.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Test Pravega Transaction Coordinator.
+# This demonstrates that decodebin will clip its output to start exactly at the seek time.
+# pravegasrc reads 34 seconds of clipped data, ensuring that all headers are received.
+# Run before: STREAM=test1 SIZE_SEC=60 scripts/videotestsrc-to-pravega-hls.sh
+
+set -ex
+ROOT_DIR=$(readlink -f $(dirname $0)/..)
+pushd ${ROOT_DIR}/gst-plugin-pravega
+cargo build
+ls -lh ${ROOT_DIR}/gst-plugin-pravega/target/debug/*.so
+export GST_PLUGIN_PATH=${ROOT_DIR}/gst-plugin-pravega/target/debug:${GST_PLUGIN_PATH}
+# log level can be INFO, DEBUG, or LOG (verbose)
+export GST_DEBUG=pravegasrc:LOG,mpegtsbase:DEBUG,mpegtspacketizer:DEBUG,h264parse:DEBUG,pravegatc:TRACE,pravegasink:LOG,INFO
+export RUST_BACKTRACE=1
+PRAVEGA_CONTROLLER=127.0.0.1:9090
+STREAM1=${STREAM1:-test1}
+STREAM2=${STREAM2:-test2}
+FPS=30
+
+gst-launch-1.0 \
+-v \
+pravegasrc name=src \
+  stream=examples/${STREAM1} \
+  controller=${PRAVEGA_CONTROLLER} \
+  start-mode=timestamp \
+  start-pts-at-zero=false \
+! identity silent=false \
+! decodebin \
+! identity silent=false \
+! pravegatc \
+! identity silent=false \
+! fakesink sync=false \
+|& tee /tmp/pravegatc3.log
